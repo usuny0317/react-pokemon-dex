e28da2d7033b4ac35abb9ca4c533ca23da678a76
usuny0317 <usuny0317@naver.com>
2025-02-04 20:22:59 +0900

    card.jsx을 안써서..쓰도록 수정했음

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,5 +1,44 @@
-const PokemonCard = () => {
-  return <div>PokemonCard</div>;
+import styled from "styled-components";
+import { addPokemon } from "../slices/pokemonsSlice";
+import { useDispatch } from "react-redux";
+
+const CardDiv = styled.div`
+  background-color: white;
+  border-radius: 3%;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
+
+  display: flex;
+  flex-direction: column;
+  place-items: center;
+  margin: 10px;
+  padding: 10px;
+  gap: 5px;
+`;
+
+const PokemonCard = (mock) => {
+  const dispatch = useDispatch();
+  return (
+    <CardDiv>
+      <img src={`${mock.img_url}`} />
+      <span>{mock.korean_name}</span>
+      <span>No. {mock.id}</span>
+      <button
+        onClick={() => {
+          dispatch(
+            addPokemon({
+              img_url: mock.img_url,
+              korean_name: mock.korean_name,
+              types: mock.types,
+              id: mock.id,
+              description: mock.description,
+            })
+          );
+        }}
+      >
+        추가
+      </button>
+    </CardDiv>
+  );
 };
 
 export default PokemonCard;
--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -1,7 +1,6 @@
-import { useDispatch } from "react-redux";
 import MOCK_DATA from "../MOCK_DATA";
-import { addPokemon } from "../slices/pokemonsSlice";
 import styled from "styled-components";
+import PokemonCard from "./PokemonCard";
 
 const ListUl = styled.div`
   display: grid;
@@ -11,49 +10,15 @@ const ListUl = styled.div`
   margin: 0 auto;
   background-color: whitesmoke;
 `;
-const ListLi = styled.div`
-  background-color: white;
-  border-radius: 3%;
-  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
-
-  display: flex;
-  flex-direction: column;
-  place-items: center;
-  margin: 10px;
-  padding: 10px;
-`;
 
 const PokemonList = () => {
   const Mocks = MOCK_DATA;
-  const dispatch = useDispatch();
+
   return (
     <div>
       <ListUl>
         {Mocks.map((mock) => {
-          return (
-            <ListLi key={mock.id}>
-              <img src={`${mock.img_url}`} />
-              <span>{mock.korean_name}</span>
-              <span>{mock.types.map((type) => type)}</span>
-              <span>No. {mock.id}</span>
-
-              <button
-                onClick={() => {
-                  dispatch(
-                    addPokemon({
-                      img_url: mock.img_url,
-                      korean_name: mock.korean_name,
-                      types: mock.types,
-                      id: mock.id,
-                      description: mock.description,
-                    })
-                  );
-                }}
-              >
-                추가
-              </button>
-            </ListLi>
-          );
+          return <PokemonCard {...mock} key={mock.id} />;
         })}
       </ListUl>
     </div>
